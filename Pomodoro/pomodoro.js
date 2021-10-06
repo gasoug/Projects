3ec5dca36697cff6ff7e60d226638d848a0088e9
gasoug <gabriel.sousago@gmail.com>
2021-10-05 23:32:38 -0300

    Adicionado o cronômetro

--- Pomodoro/pomodoro.js
+++ Pomodoro/pomodoro.js
@@ -1,41 +1,61 @@
-let second = 0
-let millisecond = 0
-let cron
+var milliseconds = "00"
+var seconds = "00"
+var minutes = "00"
+var milli = document.getElementById("milliseconds")
+var sec = document.getElementById("seconds")
+var min = document.getElementById("minutes")
+var startBtn = document.getElementById("startbt")
+var pauseBtn = document.getElementById("pausebt")
+var resetBtn = document.getElementById("resetbt")
+var pomo = document.getElementById('times').value
+var times = 0
+var interval
 
-document.form_main.start.onclick = () => start()
-document.form_main.pause.onclick = () => pause()
-document.form_main.reset.onclick = () => reset()
-
-function start(){
-    pause()
-    cron = setInterval(() =>{
-        timer()
-    }, 10)
+function startCron(){
+    milliseconds++
+    if(milliseconds < 9){
+      milli.innerHTML = "0" + milliseconds
     }
-    function pause() {
-        clearInterval(cron);
-    }  
-    function reset() {
-        //minute = 0
-        second = 0
-        millisecond = 0  
-
-        document.getElementById('second').innerText = '00';
-        document.getElementById('millisecond').innerText = '000';
+    if(milliseconds > 9){
+      milli.innerHTML = milliseconds
     }
-    
-    function timer() {
-        if ((millisecond += 10) == 1000) {
-          millisecond = 0;
-          second++;
-        }
-        /*if (second == 60) {
-          second = 0;
-          minute++;
-        }*/
-            document.getElementById('seg').innerText = returnData(second)
-            document.getElementById('miliseg').innerText = returnData(millisecond)
+    if(milliseconds > 99){
+      seconds++
+      sec.innerHTML = "0" + seconds
+      milliseconds = 0
+      milli.innerHTML = "0" + 0
     }
-    function returnData(input) {
-        return input > 10 ? input : `0${input}`
-      }
+    if(seconds > 9){
+      sec.innerHTML = seconds
+    }
+    if(seconds > 59){
+      minutes++
+      min.innerHTML = "0" + minutes
+      seconds = 0
+      sec.innerHTML = "0" + seconds
+    }
+    if(minutes > 9){
+      min.innerHTML = minutes
+    }
+}
+function started(){
+  interval = setInterval(startCron,10)
+  startBtn.disabled = true
+  pauseBtn.disabled = false
+}
+function pauseCron(){
+  clearInterval(interval)
+  startBtn.disabled = false
+  pauseBtn.disabled = true
+}
+function stopCron(){
+  clearInterval(interval)
+  milliseconds = "00"
+  seconds = "00"
+  minutes = "00"
+  milli.innerHTML = milliseconds
+  sec.innerHTML = seconds
+  min.innerHTML = minutes
+  startBtn.disabled = false
+  pauseBtn.disabled = false
+}
